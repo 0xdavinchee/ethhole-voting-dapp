085d9a2c0ac0328a5eb582c89afdc60845c8d12c
Vincent Chee <0xsquanch@Vincents-MacBook-Pro.local>
2021-04-21 20:28:11 +0800

    typescript support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,10 @@
-require("@nomiclabs/hardhat-waffle");
+import { task, HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (_args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -16,7 +17,8 @@ task("accounts", "Prints the list of accounts", async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: "0.7.3",
 };
 
+export default config;
