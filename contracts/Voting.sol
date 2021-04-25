35e140f28e0024521c615cd57f5256ea93f01b3d
Vincent Chee <vincent.kh.chee@gmail.com>
2021-04-25 16:00:12 +0800

    wip
    - initial vote logic implemented
    - getKeccak helper func implemented
    - some require clauses updated

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -32,27 +32,34 @@ contract Voting {
     locked = false;
   }
 
+  function getKeccak(string memory _string) private pure returns(bytes32) {
+    return keccak256(abi.encodePacked(_string));
+  }
+
   function startElection(uint _registrationEndPeriod, uint _votingEndPeriod) public {
     require(_registrationEndPeriod >= block.timestamp && _votingEndPeriod >= _registrationEndPeriod);
-    Election storage election = elections[numElections++];
-    election.registrationEndPeriod = _registrationEndPeriod;
-    election.votingEndPeriod = _votingEndPeriod;
+    address[] memory candidates;
+    elections.push(Election(_registrationEndPeriod, _votingEndPeriod, candidates));
   }
 
   function registerCandidate(uint _electionId, string memory _name) public {
+    require(getKeccak(_name) != getKeccak(""), "Please register with a name.");
     require(block.timestamp < elections[_electionId].registrationEndPeriod, "The registration period has ended.");
-    require(candidateToCandidateDataMap[msg.sender].electionId == 0, "You have already registered for an election.");
+    require(getKeccak(candidateToCandidateDataMap[msg.sender].name) == getKeccak(""), "You have already registered for an election.");
     elections[_electionId].candidates.push(msg.sender);
     candidateToCandidateDataMap[msg.sender] = CandidateData(0, _electionId, _name);
   }
 
-  function voteForCandidate(address _candidateId) public noReentrancy {
+  function voteForCandidate(uint _electionId, address _candidateId) public noReentrancy {
     require(!voters[msg.sender], "You have already voted for a candidate.");
-    candidateToCandidateDataMap[_candidateId] = CandidateData(
-      candidateToCandidateDataMap[_candidateId].voteCount++, 
-      candidateToCandidateDataMap[_candidateId].electionId,
-      candidateToCandidateDataMap[_candidateId].name
+    require(getKeccak(candidateToCandidateDataMap[_candidateId].name) != getKeccak(""), "This address does not belong to a candidate.");
+    require(
+      block.timestamp > elections[_electionId].registrationEndPeriod
+      && block.timestamp < elections[_electionId].votingEndPeriod,
+      "Voting is not allowed now."
     );
+    candidateToCandidateDataMap[_candidateId].voteCount++;
+    voters[msg.sender] = true;
   }
 
   function getCandidateData(address _candidateId) public view returns(uint, string memory) {
